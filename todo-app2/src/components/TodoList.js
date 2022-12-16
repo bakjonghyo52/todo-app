5d537cc86f6defbeac24d6b8144866aabb64d6e2
bakjonghyo <cutitout52@gmail.com>
2022-12-16 16:39:27 +0900

    todo-app1

---
+++ todo-app2/src/components/TodoList.js
@@ -0,0 +1,22 @@
+// src/components/TodoList.js
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+
+export default class TodoList extends Component {
+	render() {
+		const { items, handleEdit, handleDelete } = this.props;
+		return (
+			<ul className="list-group my-2">
+				{items.map((item) => {
+					return (
+						<TodoItem
+							item={item}
+							handleEdit={handleEdit}
+							handleDelete={handleDelete}
+						/>
+					);
+				})}
+			</ul>
+		);
+	}
+}
---
+++ todo-app2/src/index.js
@@ -0,0 +1,8 @@
+// src/index.js
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
